91ceaf172de5191c02071443104e9444185bd690
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-02-04 12:15:52 +0300

    Add files via upload

--- html-element-collection/audioplayer/js/audioplayer.js
+++ html-element-collection/audioplayer/js/audioplayer.js
@@ -4,12 +4,11 @@ let audioArr = ['https://netology-code.github.io/hj-homeworks/html-element-colle
                 'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA Fusion Jam.mp3'];
 let mediaplayer = document.getElementsByClassName('mediaplayer')[0];
 let audioFile = mediaplayer.getElementsByTagName('audio')[0];
-
 let playState = document.getElementsByClassName('playstate')[0];
 let playBtn = playState.getElementsByClassName('fa-play')[0];
 let stopBtn = document.getElementsByClassName('stop')[0];
 let forwardBtn = document.getElementsByClassName('next')[0];
-let BackwardBtn = document.getElementsByClassName('back')[0];
+let backwardBtn = document.getElementsByClassName('back')[0];
 let audioFileTitle = document.getElementsByClassName('title')[0];
 
 playState.onclick = function() {
@@ -22,13 +21,15 @@ playState.onclick = function() {
         audioFile.play();
     }
 }
+
 stopBtn.onclick = function() {
     audioFile.pause();
     mediaplayer.classList.remove('play');
     audioFile.currentTime = 0;
 }
 
-let position = 0; 
+let position = 0;
+ 
 forwardBtn.onclick = function () {
     position += 1;
     if (position == (audioArr.length)) {
@@ -39,7 +40,8 @@ forwardBtn.onclick = function () {
     mediaplayer.classList.add('play');
     audioFileTitle.title = audioFile.src.substring( audioFile.src.lastIndexOf("/") + 1, audioFile.src.lastIndexOf(".") ).replace(/[%20]{3,3}/g, ' ');
 } 
-BackwardBtn.onclick = function () {
+
+backwardBtn.onclick = function () {
     position -= 1;
     if (position < 0) {
         position = audioArr.length - 1;
