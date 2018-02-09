eabda0b9d91fcc516e8848b628fe82e5998cdb3f
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-02-09 07:03:30 +0300

    Add files via upload

--- event-object/piano/js/piano.js
+++ event-object/piano/js/piano.js
@@ -1,5 +1,6 @@
-'use strick'
+'use strict'
 let buttons = document.getElementsByTagName('li');
+
 let play = function () {
     if (document.getElementsByTagName('ul')[0].className == 'lower') {
         this.getElementsByTagName('audio')[0].src = this.getElementsByTagName('audio')[0].src.replace('middle', 'lower');
@@ -15,14 +16,18 @@ for (const button of buttons) {
 }
 
 document.addEventListener('keypress', (event) => {
-    if (event.code == 'KeyS') {
-        document.getElementsByTagName('ul')[0].className = 'lower';
-    }
-    if (event.code == 'KeyA') {
-        document.getElementsByTagName('ul')[0].className = 'higher';
-    }
+    console.log(`зажата`);
 })
 
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'Shift') {
+        document.getElementsByTagName('ul')[0].className = 'lower';
+    }
+    if (event.key == 'Alt') {
+        document.getElementsByTagName('ul')[0].className = 'higher';
+    }
+});
+ 
 document.addEventListener('keyup', (event) => {
     document.getElementsByTagName('ul')[0].className = 'set middle';
     Array.from(document.getElementsByTagName('audio')).forEach((event) => {
