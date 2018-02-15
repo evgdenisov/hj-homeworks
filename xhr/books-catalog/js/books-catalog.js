da590b31bd06a1a8a8425eb822e9ce70b02aa34a
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-02-15 10:28:15 +0300

    Add files via upload

---
+++ xhr/books-catalog/js/books-catalog.js
@@ -0,0 +1,21 @@
+'use strict'
+let xhr = new XMLHttpRequest();
+
+xhr.open(
+    "GET",
+    "https://neto-api.herokuapp.com/book/",
+    true
+)
+xhr.send();
+xhr.addEventListener('load', onLoad);
+
+function onLoad() {
+    document.getElementById('content').innerHTML = '';
+    JSON.parse(xhr.responseText).forEach(function(book) {
+        document.getElementById('content').innerHTML += '<li data-title="' + book.title + '" data-author="'
+                    + book.author.name + '" data-info="' + book.info + '" data-price="' + book.price + '">' +
+                    '<img src="' + book.cover.small + '"></img></li>';
+    });
+}
+
+document.getElementById('content')
