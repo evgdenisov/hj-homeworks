884093d713fe8b4ccf42a46f0e92fe331897acd1
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-02-17 13:42:46 +0300

    Add files via upload

--- xhr/books-catalog/js/books-catalog.js
+++ xhr/books-catalog/js/books-catalog.js
@@ -9,11 +9,13 @@ xhr.open(
 xhr.send();
 xhr.addEventListener('load', onLoad);
 
+function itemCreator(book) {
+    document.getElementById('content').innerHTML += '<li data-title="' + book.title + '" data-author="'
+    + book.author.name + '" data-info="' + book.info + '" data-price="' + book.price + '">' +
+    '<img src="' + book.cover.small + '"></li>';
+}
+
 function onLoad() {
     document.getElementById('content').innerHTML = '';
-    JSON.parse(xhr.responseText).forEach(function(book) {
-        document.getElementById('content').innerHTML += '<li data-title="' + book.title + '" data-author="'
-                    + book.author.name + '" data-info="' + book.info + '" data-price="' + book.price + '">' +
-                    '<img src="' + book.cover.small + '"></img></li>';
-    });
+    JSON.parse(xhr.responseText).forEach(itemCreator);
 }
