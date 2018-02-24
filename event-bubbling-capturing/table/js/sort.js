e1392295144dd70c3ddae4f31df1f436dfbd79af
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-02-24 18:05:03 +0300

    Add files via upload

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,24 @@
 'use strict';
+let tHeadDir = document.querySelector('thead').dataset.dir;
+let targetName = '';
+
 
 function handleTableClick(event) {
-  
+    if (event.target.dataset.propName != targetName) {
+        tHeadDir = 1;
+    }
+    if (tHeadDir === undefined) {
+        tHeadDir = 1;
+    }
+    targetName = event.target.dataset.propName;
+    sortTable(event.target.dataset.propName, tHeadDir);
+    table.dataset.sortBy = event.target.dataset.propName;
+    if (tHeadDir == 1) {
+        tHeadDir = -1;
+        return;
+    }
+    if (tHeadDir == -1) {
+        tHeadDir = 1;
+        return;
+    }
 }
