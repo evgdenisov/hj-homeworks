c777ddae0ad53aa3b85f91c807f0ff4fa5c7a5d9
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-02-24 18:04:16 +0300

    Add files via upload

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let btns = document.querySelectorAll('.add-to-cart');
+
+function btnEvent(event) {
+    event.preventDefault();
+    let item = event.target;
+    item.title = event.target.dataset.title;
+    item.price = event.target.dataset.price;
+    addToCart(item);
+}
+
+function addBtns() {
+    btns = document.querySelectorAll('.add-to-cart');
+    for (const btn of btns) {
+        btn.addEventListener('click', btnEvent);
+    }
+}
+
+addBtns();
+document.querySelector('.show-more').addEventListener('click', addBtns);
