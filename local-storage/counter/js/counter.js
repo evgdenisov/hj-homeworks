d30661416eef9983406275a0e800477a4e62a3e7
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-03-03 14:41:04 +0300

    Add files via upload

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,44 @@
 'use strict';
+let increment = document.getElementById('increment');
+let decrement = document.getElementById('decrement');
+let reset = document.getElementById('reset');
+let buttons = Array.from(document.querySelectorAll('.wrap-btns button'));
+let counter = 0;
+
+function outputText () {
+    localStorage.counter = counter;
+    document.getElementById('counter').textContent = localStorage.counter;
+}
+
+function check() {
+    if (localStorage.counter === undefined) {
+        localStorage.counter = 0;
+    }
+    else {
+        counter = Number(localStorage.counter);
+    }
+}
+
+function buttonEvent(event) {
+    if (event.currentTarget == increment) {
+        counter += 1;
+    }
+    if (event.currentTarget == decrement) {
+        if (counter === 0) {
+            return;
+        }
+        counter -= 1;
+    }
+    if (event.currentTarget == reset) {
+        counter = 0;
+    }
+    outputText();
+}
+
+for (const button of buttons) {
+    button.addEventListener('click', buttonEvent);
+}
+
+check();
+outputText();
+
