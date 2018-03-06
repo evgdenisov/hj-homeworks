7db05619e685176d420f54dfe802efa0bef005a3
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-03-06 18:21:35 +0300

    Add files via upload

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connection.addEventListener('message', (event) => {
+    let data = JSON.parse(event.data);
+    document.querySelector('.counter').innerHTML = data.connections;
+    document.querySelector('.errors').innerHTML = data.errors;
+});
+
+window.addEventListener('beforeunload', () => {
+    connection.close(1000, 'connection close');
+});
+
