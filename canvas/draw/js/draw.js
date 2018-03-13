f6ae397f91a0df44961afa5c120ddc1555a659fe
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-03-13 20:17:36 +0300

    Add files via upload

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,1 +1,91 @@
 'use strict';
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+let counter = 0;
+let shift = false;
+let lineWidth = 'down';
+let size = 100;
+
+function resize() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+}
+
+function tick() {
+    if (shift) {
+        if (counter == 0) {
+            counter = 359;
+        }
+        else {
+            counter --;
+        }
+    } 
+    else {
+        if (counter == 359) {
+            counter = 0;
+        }
+        else {
+            counter++;
+        }
+    }
+    if (lineWidth == 'up') {
+        if (size == 99) {
+            lineWidth = 'down';
+        }
+        size++;
+    }
+    else {
+        if (size == 6) {
+            lineWidth = 'up';
+        }
+        size--;
+    }
+    window.setTimeout(tick, 1000);
+}
+
+function draw(event) {
+    ctx.lineWidth = size;
+    ctx.strokeStyle = `hsl(${counter}, 100%, 50%)`;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.lineTo(event.layerX, event.layerY);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.shiftKey) {
+        shift = true;
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    if (!event.shiftKey) {
+        shift = false;
+    }
+    
+})
+
+window.addEventListener('resize', resize);
+
+canvas.addEventListener('mousedown', (event) => {
+    draw(event);
+    canvas.addEventListener('mousemove', draw);
+});
+
+canvas.addEventListener('mouseup', (event) => {
+    canvas.removeEventListener('mousemove', draw);
+});
+
+canvas.addEventListener('mouseleave', () => {
+    canvas.removeEventListener('mousemove', draw);
+});
+
+canvas.addEventListener('dblclick', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
+
+tick();
+resize();
