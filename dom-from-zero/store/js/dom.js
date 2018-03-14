80eec9544ecd1d5d2668a91a20a31b664be1a379
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-03-14 19:06:28 +0300

    Add files via upload

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,27 @@
 'use strict';
+
+
+function createElement(content) {
+  const element = document.createElement(content.name);
+  
+  if ((typeof content == 'string') || (typeof content == 'number')) {
+    return document.createTextNode(content);
+  }
+  if ((content === undefined) || (content === null)) {
+    return document.createTextNode('');
+  }
+
+  if (content.props && Object.keys(content.props).length > 0) {
+    Object.keys(content.props).forEach(key => {
+      if (content.props[key]) {
+        element.setAttribute(key, content.props[key]);
+      }
+    });
+  }
+  if (content.childs && content.childs.length > 0) {
+    content.childs.forEach(child => {
+      element.appendChild(createElement(child));
+    })
+  }
+  return element;
+}
