31e148f29e28662bc2baf4d7a0e43fbb828fa9b2
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-03-18 21:15:03 +0300

    Add files via upload

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+function onUpdate(event) {
+    const canvas = event.canvas;
+    canvas.toBlob(sendBlob);
+}
+
+function sendBlob(blob) {
+    ws.send(blob);
+}
+
+ws.addEventListener('message', event => {
+    console.log(event.data);
+});
+
+ws.addEventListener('open', () => {
+    console.log('opened')
+    window.editor.addEventListener('update', onUpdate);
+});
