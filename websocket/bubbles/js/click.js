e71b80066ec014d556edb103b3aa5b06ab86c469
evgdenisov <31406596+evgdenisov@users.noreply.github.com>
2018-04-05 08:57:55 +0300

    Add files via upload

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,24 @@
 'use strinct';
+
+let connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+function onClick(event) {
+    let clickInfo = {
+        x : event.x,
+        y : event.y
+    }
+    connection.send(JSON.stringify(clickInfo));
+}
+
+function onError(er) {
+    console.log(`Возникла ошибка ${er.data}`);
+}
+
+function onUnload() {
+    connection.close();
+}
+
+showBubbles(connection);
+document.addEventListener('click', onClick);
+connection.addEventListener('error', onError);
+window.addEventListener('beforeunload', onUnload);
